055f1a6efb1e0c8c472b214d935cc3ae8e9dfc8f
Prakash-1619 <poojariprakash88@gmail.com>
2026-07-16 00:48:50 +0530

    Comment out the Merging module in STAGE_MODULES

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def load_config():
 STAGE_MODULES = {
     #"Ingestion": "src.ingestion.run",
     #"Cleaning": "src.cleaning.run",
-    "Merging": "src.merging.run",
+    #"Merging": "src.merging.run",
     "Modeling": "src.modeling.run",
     "Forecasting": "src.forecasting.run",
     "Forecasting_news": "src.forecasting_news.run",
